bd324c7c7648e70a4f42d64e537b16a7f23a66b2
Sakib Hasan <sakib100.sa@gmail.com>
2023-01-09 14:54:02 +0600

    adding login page to the project

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,24 @@
-import "./login.scss"
+import "./login.scss";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	return (
+		<div className="login">
+			<form>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Email"
+				/>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="Password"
+				/>
+			</form>
+		</div>
+	);
+};
 
-export default Login
+export default Login;
