ae667fcffa61d151f13c090192454ec3acf1df97
Sakib Hasan <sakib100.sa@gmail.com>
2023-01-11 22:45:08 +0600

    adding requireAuth to route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
@@ -11,7 +11,10 @@ import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
 	const { darkMode } = useContext(DarkModeContext);
-
+	const currentUser = false;
+	const RequireAuth = ({ children }) => {
+		return currentUser ? children : <Navigate to="/login" />;
+	};
 	return (
 		<div className={darkMode ? "app dark" : "app"}>
 			<BrowserRouter>
